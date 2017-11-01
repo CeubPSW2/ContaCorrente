6ea79289650fbebc3db88c4cf9cb3be48104b748
Alexandre.Torres <Alexandre.Torres@AN01017MM074664.dinfor.uniceub>
2017-11-01 19:35:36 -0200

    Completo para o exercício

--- ContaCorrente/src/contacorrente/Controle.java
+++ ContaCorrente/src/contacorrente/Controle.java
@@ -11,6 +11,7 @@ import model.Cliente;
 import model.ContaCorrente;
 import model.Credito;
 import model.Debito;
+import org.hibernate.SessionFactory;
 import service.ClienteService;
 
 /**
@@ -19,6 +20,9 @@ import service.ClienteService;
  */
 public class Controle {
     public static void main(String[] args) {
+        menuPrincipal();
+    }
+    public static void menuPrincipal() {
         Scanner teclado = new Scanner(System.in);
         
         boolean sair = false;
@@ -26,13 +30,12 @@ public class Controle {
             System.out.println("O que deseja fazer?");
             System.out.println("1. Inicializar dados");
             System.out.println("2. Consultar clientes");
-            System.out.println("3. Consultar lançamentos de um cliente");
             System.out.println("0. Sair");
             int opcao = teclado.nextInt();
             switch( opcao){
                 case 1: inicializa();
                         break;
-                case 2: consultar();
+                case 2: consultarClientes();
                         break;
                 case 0: sair = true;
                         break;
@@ -40,7 +43,79 @@ public class Controle {
             }
         }while(!sair);
         
+        try {
+            SessionFactory sf = HibernateUtil.getSessionFactory();
+            sf.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
     }
+
+    public static void menuMovimentacao() {
+        Scanner teclado = new Scanner(System.in);
+        
+        boolean sair = false;
+        do{
+            System.out.println("O que deseja fazer?");
+            System.out.println("1. Consultar conta do cliente");
+            System.out.println("2. Consultar movimentações do cliente");
+            System.out.println("3. Lançar débito");
+            System.out.println("4. Lançar crédito");
+            System.out.println("0. voltar");
+            int opcao = teclado.nextInt();
+            
+            switch( opcao){
+                case 1: consultaConta();
+                        break;
+                case 2: consultaMovimentacao();
+                        break;
+                case 3: lancarDebito();
+                        break;
+                case 4: lancarCredito();
+                        break;
+                case 0: sair = true;
+                        break;
+                default: System.out.println("Opção inválida");
+            }
+        }while(!sair);
+        
+    }    
+    public static void consultaConta(){
+        /*
+         TODO
+        1 - pedir o id do cliente e recuperar o objeto
+        2 - criar ContaCorrenteService.getContaCorrente(Cliente cliente)
+        3 - mostrar o saldo da conta corrente
+        */       
+    }
+    
+    public static void consultaMovimentacao(){
+        /*
+         TODO
+        1 - pedir o id do cliente e recuperar o objeto
+        2 - criar MovimentacaoService.getMovimentacao(Cliente cliente)
+        3 - mostrar os lançamentos, destacando se débito ou crédito
+        */               
+    }
+
+    public static void lancarDebito(){
+        /*
+         TODO
+        1 - pedir o id do cliente e recuperar o objeto
+        2 - pedir o valor do débito
+        2 - criar MovimentacaoService.lancarDebito(Cliente cliente, double valor)
+        */               
+    }    
+    
+    public static void lancarCredito(){
+        /*
+         TODO
+        1 - pedir o id do cliente e recuperar o objeto
+        2 - pedir o valor do crédito
+        2 - criar MovimentacaoService.lancarCredito(Cliente cliente, double valor)
+        */               
+    }    
     
     public static void inicializa() {
         
@@ -64,13 +139,20 @@ public class Controle {
         ClienteService.insere( cliente2 );        
     }    
     
-    public static void consultar(){
+    public static void consultarClientes(){
         
         List<Cliente> clientes = ClienteService.getClientes();
             
         System.out.println("Lista de clientes\n------------------------");
         for (Cliente c : clientes)
             System.out.println(c);
+        
+        menuMovimentacao();
+        /* TODO
+        - pegar o Id e obter o cliente
+        - fazer o getMovimentacoes passando o cliente
+        - 
+        */
     }    
     
 }
